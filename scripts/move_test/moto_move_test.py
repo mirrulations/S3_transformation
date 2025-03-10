3b7efe5512265148dba543368dd7fed4b5f95227
Saul Sanchez <sanchezs@moravian.edu>
2025-03-10 14:24:16 -0400

    slight changes to files to be properly working with current repo

--- scripts/move_test/moto_move_test.py
+++ scripts/move_test/moto_move_test.py
@@ -15,7 +15,7 @@ When running this file, you need to make sure to add S3. in front of the where o
 # Add the path to the transformation_trigger directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-from S3.move import (
+from scripts.move import (
     create_placeholder,
     create_raw_data_folder,
     move_object,
